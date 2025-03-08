849be4ab7baa49445ff21b85d80e1d9eadfd13ac
ervin arviandi <muhamadervin34@gmail.com>
2025-03-08 15:46:47 +0700

    update width & height pada webcam

--- components/commons/Photobooth.tsx
+++ components/commons/Photobooth.tsx
@@ -104,7 +104,7 @@ const handlechangeColor = useCallback((e: React.ChangeEvent<HTMLInputElement>) =
                 screenshotFormat="image/jpeg"
                 videoConstraints={videoConstraints}
                 width={640}
-                height={640}
+                height={480}
                 style={{ maxWidth: '100%', height: 'auto', borderRadius: '10px' 
                 }}
               />
@@ -129,7 +129,7 @@ const handlechangeColor = useCallback((e: React.ChangeEvent<HTMLInputElement>) =
               <div className='w-full '>
                 {capturedPhotos.map((photo, index) => (
                   <div className='p-3 rounded-lg ' key={index} >
-                    <Image src={photo}  alt={`Captured ${index + 1}`}  />
+                    <Image src={photo} width={640} height={480}  alt={`Captured ${index + 1}`}  />
                     {/* <p>Photo {index + 1}</p> */}
                   </div>
                 ))}
